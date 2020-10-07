0b9cf61c5b5b9d05476db608f130dc9d9c0afa0b
Aleksei V <al.voitovskii@gmail.com>
2020-10-07 21:55:41 +0300

    feat: establish database connection

---
+++ db/config.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const database = 'Todo'
+const password = '*@Qysyhex!1'
+const uri = `mongodb+srv://BewitchedBySycorax:${password}@cluster0.iqo9r.mongodb.net/${database}?retryWrites=true&w=majority`
+
+// It lets mongoose call the createIndex method on the mongodb native driver
+mongoose.set('useCreateIndex', true)
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    mongoose.connection.on('error', e => {
+      process.stderr.write(e)
+    })
+  } catch (e) {
+    process.stdout.write(e.message)
+  }
+}
+
+module.exports = dbConnect
