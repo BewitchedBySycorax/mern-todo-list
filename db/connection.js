0f0317963b266dcd533876b065eac58d267833d9
Aleksei V <al.voitovskii@gmail.com>
2020-10-08 02:10:07 +0300

    feat: rename 'config.js' to 'connection.js'

--- db/connection.js
+++ db/connection.js
@@ -7,19 +7,17 @@ const uri = `mongodb+srv://BewitchedBySycorax:${password}@cluster0.iqo9r.mongodb
 // It lets mongoose call the createIndex method on the mongodb native driver
 mongoose.set('useCreateIndex', true)
 
-const dbConnect = async () => {
+const connection = async () => {
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     })
-    mongoose.connection.on('error', e => {
-      process.stderr.write(e)
-    })
+    mongoose.connection.on('error', e => process.stderr.write(e))
   } catch (e) {
     process.stdout.write(e.message)
   }
 }
 
-module.exports = dbConnect
+module.exports = connection
