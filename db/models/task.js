4c287c796bea838c22b3e73d05c7d0d370d6ccf8
Aleksei V <al.voitovskii@gmail.com>
2020-10-17 23:54:44 +0300

    fix: remove unnecessary 'deadline'

--- db/models/task.js
+++ db/models/task.js
@@ -6,7 +6,6 @@ const taskSchema = new Schema(
   {
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
-    deadline: { type: Date, default: null },
     user: { type: Schema.ObjectId, ref: 'User' } // User-Task binding (User model)
   }, {
     versionKey: false
