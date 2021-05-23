6b4f256a56f7ba96c214eda611cf8ee89ad18b5e
Aleksei Voitovskii <al.voitovskii@gmail.com>
2021-05-24 02:48:35 +0300

    BREAKING CHANGE: only jwt onwards

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-/* eslint-disable no-unused-expressions */
-const auth = require('./api/auth')
-const register = require('./api/register')
-const tasks = require('./api/tasks')
+/* eslint-disable global-require */
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-module.exports = app => {
-  auth(app)
-  register(app)
-  tasks(app)
-}
+const checkAuthentication = require('./middlewares/auth')
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+require('./db/connection')() // Database connection
+
+app.use(bodyParser.urlencoded({ extended: true })) // if false - either string or array in req.body
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.use('/tasks', checkAuthentication)
+
+require('./api/index')(app) // API
+
+app.get('/', (_req, res) => res.sendFile(path.resolve(__dirname, 'client', 'index.html')))
+app.get('/auth', (_req, res) => res.sendFile(path.resolve(__dirname, 'client', 'auth.html')))
+
+app.listen(PORT, () => console.log(`Server has been started on port:${PORT}`))
